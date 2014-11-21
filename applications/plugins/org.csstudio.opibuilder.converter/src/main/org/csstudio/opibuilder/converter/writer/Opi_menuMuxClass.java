0cc8e4de9a32f763d80ce53ceee1e62caeddd963
Nick Battam <nicholas.battam@diamond.ac.uk>
2014-11-21 15:12:01 +0000

    o.c.opibuilder: Rename muxmenu as menumux

--- applications/plugins/org.csstudio.opibuilder.converter/src/main/org/csstudio/opibuilder/converter/writer/Opi_menuMuxClass.java
+++ applications/plugins/org.csstudio.opibuilder.converter/src/main/org/csstudio/opibuilder/converter/writer/Opi_menuMuxClass.java
@@ -12,7 +12,7 @@ import org.w3c.dom.Element;
 public class Opi_menuMuxClass extends OpiWidget {
 
 	private static Logger log = Logger.getLogger("org.csstudio.opibuilder.converter.writer.Opi_menuMuxClass");
-	private static final String typeId = "edm.muxmenu";
+	private static final String typeId = "edm.menumux";
 	private static final String name = "EDM MenuMux";
 	private static final String version = "1.0";
 
