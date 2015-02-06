285cf9a9535611b6919b0f0b5a9506f002d9af6c
Matthew Furseman <matthew.furseman@diamond.ac.uk>
2015-02-06 15:13:29 +0000

    o.c.opibuilder: Catch infinite loop in StringProperties macro expansion

--- applications/plugins/org.csstudio.opibuilder/src/org/csstudio/opibuilder/util/OPIBuilderMacroUtil.java
+++ applications/plugins/org.csstudio.opibuilder/src/org/csstudio/opibuilder/util/OPIBuilderMacroUtil.java
@@ -7,6 +7,8 @@
  ******************************************************************************/
 package org.csstudio.opibuilder.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.csstudio.apputil.macros.IMacroTableProvider;
@@ -17,6 +19,8 @@ import org.csstudio.opibuilder.model.AbstractWidgetModel;
 import org.csstudio.opibuilder.preferences.PreferencesHelper;
 
 
+
+
 /**Selfdefined MacroUtil for opibuilder.
  * @author Xihui Chen
  *
@@ -25,6 +29,9 @@ public class OPIBuilderMacroUtil {
 	public static final String DNAME = "DNAME"; //$NON-NLS-1$
 	public static final String DID = "DID"; //$NON-NLS-1$
 	public static final String DLOC = "DLOC"; //$NON-NLS-1$
+
+	private static Cache cache;
+
 	/**Replace the macros in the input with the real value.  Simply calls the three argument version below
 	 * @param input the raw string which include the macros string $(macro)
 	 * @return the string in which the macros have been replaced with the real value.
@@ -32,7 +39,10 @@ public class OPIBuilderMacroUtil {
 	public static String replaceMacros(AbstractWidgetModel widgetModel, String input){
 		
 		try {
-			return MacroUtil.replaceMacros(input, new WidgetMacroTableProvider(widgetModel));
+			if(cache == null || !cache.contains(widgetModel)) {
+				cache = new Cache(widgetModel, new WidgetMacroTableProvider(widgetModel));
+			}
+			return MacroUtil.replaceMacros(input, cache.getValue());
 		} catch (InfiniteLoopException e) {
 			ConsoleService.getInstance().writeWarning(e.getMessage());
 			return input;
@@ -68,16 +78,26 @@ public class OPIBuilderMacroUtil {
 class WidgetMacroTableProvider implements IMacroTableProvider{
 	private AbstractWidgetModel widgetModel;
 	private Map<String, String> macroMap;
+	private List<String> unparsedMacros;
+
 	public WidgetMacroTableProvider(AbstractWidgetModel widgetModel) {
 		this.widgetModel = widgetModel;
+		this.unparsedMacros = new ArrayList<String>();
 		macroMap = OPIBuilderMacroUtil.getWidgetMacroMap(widgetModel);
 	}
 	
-	public String getMacroValue(String macroName) {	
+	public String getMacroValue(String macroName) {
 		if(macroMap != null && macroMap.containsKey(macroName))
 			return macroMap.get(macroName);
 		else if(widgetModel.getAllPropertyIDs().contains(macroName)){
+			// Catch an infinite loop in macro name expansion
+			if(unparsedMacros.contains(macroName)) {
+				return null;
+			}
+			unparsedMacros.add(macroName);
+			// This call can refer back to this method when StringProperties contains recursive macros
 			Object propertyValue = widgetModel.getPropertyValue(macroName);
+			unparsedMacros.remove(macroName);
 			if(propertyValue != null)
 				return propertyValue.toString();
 		}		
@@ -96,5 +116,23 @@ class WidgetMacroTableProvider implements IMacroTableProvider{
 		}
 		
 		return null;
+	}
+}
+
+class Cache {
+	final private AbstractWidgetModel key;
+	final private WidgetMacroTableProvider value;
+
+	public Cache(AbstractWidgetModel key, WidgetMacroTableProvider value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public boolean contains(AbstractWidgetModel model) {
+		return this.key.equals(model);
+	}
+
+	public WidgetMacroTableProvider getValue() {
+		return this.value;
 	}
 }
