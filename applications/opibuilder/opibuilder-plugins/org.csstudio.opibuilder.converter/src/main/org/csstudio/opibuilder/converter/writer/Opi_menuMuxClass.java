fd8508dd06cfbaaf046295000664860215096ea5
Will Rogers <wahrogers@gmail.com>
2015-09-18 13:13:30 +0100

    o.c.o.converter: only add MenuMux control PV if present.

--- applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.converter/src/main/org/csstudio/opibuilder/converter/writer/Opi_menuMuxClass.java
+++ applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.converter/src/main/org/csstudio/opibuilder/converter/writer/Opi_menuMuxClass.java
@@ -29,7 +29,9 @@ public class Opi_menuMuxClass extends OpiWidget {
 
         Element widget = widgetContext.getElement();
 
-        new OpiString(widgetContext, "pv_name", convertPVName(r.getControlPv()));
+        if (r.getControlPv() != null) {
+            new OpiString(widgetContext, "pv_name", convertPVName(r.getControlPv()));
+        }
 
         if (r.getAttribute("initialState").isExistInEDL()) {
             new OpiString(widgetContext, "initial", r.getInitialState());
