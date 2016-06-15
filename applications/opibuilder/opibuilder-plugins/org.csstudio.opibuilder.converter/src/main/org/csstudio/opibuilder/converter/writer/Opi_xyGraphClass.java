87d4967e8c800389933da6f18f4e6fcfdba91d22
Nick Battam <nicholas.battam@diamond.ac.uk>
2016-06-15 13:24:33 +0100

    Remove debug prints

--- applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.converter/src/main/org/csstudio/opibuilder/converter/writer/Opi_xyGraphClass.java
+++ applications/opibuilder/opibuilder-plugins/org.csstudio.opibuilder.converter/src/main/org/csstudio/opibuilder/converter/writer/Opi_xyGraphClass.java
@@ -255,7 +255,7 @@ public class Opi_xyGraphClass extends OpiWidget {
             for (Entry<String, EdmString> entry : r.getPlotStyle().getEdmAttributesMap().entrySet()) {
                 final String value = entry.getValue().get();
                 final String key = entry.getKey();
-                System.out.println(key + ": " + value);
+
                 if(value.equals("needle")){
                     new OpiInt(widgetContext, String.format(TRACE_N_TRACE_TYPE, key), BAR);
                 } else if (value.equals("point")) {
@@ -289,7 +289,6 @@ public class Opi_xyGraphClass extends OpiWidget {
         if(r.getPlotSymbolType().isExistInEDL()){
             for (Entry<String, EdmString> entry : r.getPlotSymbolType().getEdmAttributesMap().entrySet()) {
 
-                System.out.println(entry.getValue().get());
                 int style = STYLE_NONE;
                 if (entry.getValue().get().equals("circle")) {
                     style = STYLE_CIRCLE;
